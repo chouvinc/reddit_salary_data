589b876f682212b180cc21e8f92b786359347371
Vincent Chou <vin.chou@mail.utoronto.ca>
2018-05-30 22:26:47 -0400

    finally figured out how to do something recursively again

--- salary_grabber.py
+++ salary_grabber.py
@@ -3,17 +3,20 @@ import json, requests, pprint
 class SalaryGrabber:
     salaries = []
     urls = []
-    jsons =[]
-    pp = pprint.PrettyPrinter(indent=4)
+    jsons = []
+    text_bodies = []
 
     def __init__(self, urls):
         self.salaries = []
         self.urls = urls
+        
+        # list of jsons from salary thread
         self.jsons = []
         
         # get json from urls
         self.get_JSON(urls)
 
+    # JSON Methods
     def get_JSON(self, urls):
         for url in urls:
             json = requests.get(
@@ -21,16 +24,36 @@ class SalaryGrabber:
                 headers={'user-agent': 'Mozilla/5.0'}
             )
 
-            self.jsons.append(json)
+            # need to call .json() after since above returns requests
+            self.jsons.append(json.json())
     
     def print_JSON(self):
         for item in self.jsons:
             print(item.json())
 
-    def obj_keys(self):
-        first_item = self.jsons[0].json()[0]
+    def save_JSON(self):
+        self.r_find(self.jsons, 'body')
+      
+        with open('salary_thread', 'wt') as f:
+            f.write(str(pprint.pformat(self.text_bodies)))
 
-        for key, value in first_item['data'].items():
-            self.pp.pprint(key)
+
+    def read_existing_JSON(self):
+        # Assumes a well-formatted dict as a string in the JSON file
+        with open('salary_thread') as f:
+            print(f.read())
+
+    def r_find(self, l_or_d, key):
+        # recursively search a nested list/dict for a field
+
+        if isinstance(l_or_d, list):
+            for i in l_or_d:
+                self.r_find(i, key)
+        elif isinstance(l_or_d, dict):
+            if key in l_or_d: 
+                self.text_bodies.append(l_or_d[key])       
+            for values in l_or_d.values():
+                self.r_find(values, key)
+
+
         
-            
--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from salary_grabber import SalaryGrabber
 
 salaries = SalaryGrabber(['https://www.reddit.com/r/cscareerquestions/comments/6ye3fs/official_salary_sharing_thread_for_new_grads.json'])
-salaries.obj_keys()
+salaries.save_JSON()
