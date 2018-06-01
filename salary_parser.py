ef11186a2e8973c62ee8ade125c693c94f234ea8
Vincent Chou <vin.chou@mail.utoronto.ca>
2018-05-31 22:59:44 -0400

    deleting stuff

--- salary_parser.py
+++ salary_parser.py
@@ -1,25 +1,31 @@
 # Looks into datasets for some data to parse, sanatize, and possibly reformat if needed
 
 class SalaryParser:
+    # string that specifies current dataset
+    file_name = ''
+    full_path = ''
+    data = []
 
-    def __init__(self, path="datasets/"):
+    def __init__(self, path="datasets/", file_name=""):
         self.path = path
+        self.file_name = file_name
 
-    # method to sanitize file of non-alphanumeric values
-    def sanitize(self):
-        # get the file from the datasets folder and read
-        data = ''
-        with open('datasets/salary_thread') as f:
-            data = f.read()
+        if file_name:
+            self.full_path = path + file_name
 
-        # do stuff with the dataset
-        data.rstrip()
+    # method to grab a particular dataset to parse
+    # TODO: remove default for file_name after figuring out what to do if not given file
+    def set_dataset(self, file_name='salary_thread'):
+        with open('datasets/' + file_name) as f:
+            new_data = []
 
-        # write it back to the file for future use
-        with open('datasets/salary_thread') as f:
-            f.write(data)
-
-
+            for line in f:
+                new_data.append(line)
+            
+            self.data = new_data
+        
+        self.file_name = file_name
+        self.full_path = self.path + file_name
 
     # method to parse and format as list of key-value pairs <key: text_body>
     def parse_and_dict(self):
--- start.py
+++ start.py
@@ -1,4 +1,8 @@
 from salary_grabber import SalaryGrabber
+from salary_parser import SalaryParser
 
 salaries = SalaryGrabber(['https://www.reddit.com/r/cscareerquestions/comments/6ye3fs/official_salary_sharing_thread_for_new_grads.json'])
 salaries.save_JSON()
+
+parser = SalaryParser()
+parser.set_dataset()
