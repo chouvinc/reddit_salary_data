523ec5d8871364e97fbb3a76f89e4019f43bc8d1
Vincent Chou <vincentchou@Vincents-MacBook-Pro.local>
2018-05-31 22:04:13 -0400

    secret in class updates

--- salary_parser.py
+++ salary_parser.py
@@ -6,8 +6,20 @@ class SalaryParser:
         self.path = path
 
     # method to sanitize file of non-alphanumeric values
-    def sanatize(self):
-        pass
+    def sanitize(self):
+        # get the file from the datasets folder and read
+        data = ''
+        with open('datasets/salary_thread') as f:
+            data = f.read()
+
+        # do stuff with the dataset
+        data.rstrip()
+
+        # write it back to the file for future use
+        with open('datasets/salary_thread') as f:
+            f.write(data)
+
+
 
     # method to parse and format as list of key-value pairs <key: text_body>
     def parse_and_dict(self):
